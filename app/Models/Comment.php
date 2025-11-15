ed04d52c50d9b91cbb53e754e53706899c539386
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-11-15 14:42:10 +0000

    Chore: journal model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
-    //
+    public function journal(): BelongsTo
+    {
+        return $this->belongsTo(Journal::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
