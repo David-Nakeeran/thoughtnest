1c2ea1af9a9a8b9ab33815bdc114ab52bb0e1e2d
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-11-19 17:12:20 +0000

    Feat: basic user reg form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,14 @@
 <x-layout>
-    <h1>register page</h1>
+    <form method="POST" action="/register" class="flex flex-col">
+        @csrf
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" required>
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" required>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" required>
+        <label for="password_confirmation">Confirm Password</label>
+        <input type="password" id="password_confirmation" name="password_confirmation" required>
+        <button type="submit">Register</button>
+    </form>
 </x-layout>
