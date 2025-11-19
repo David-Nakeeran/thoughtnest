d7502d0f46ae5d33411562adc6fded99a2fc53f1
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-11-19 17:10:55 +0000

    Chore: user register controller

---
+++ app/Http/Controllers/RegisteredUserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RegisteredUserController extends Controller
+{
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    public function store(RegisterUserRequest $request)
+    {
+        $user = User::create($request->validated());
+        Auth::login($user);
+
+        return redirect('/');
+    }
+}
