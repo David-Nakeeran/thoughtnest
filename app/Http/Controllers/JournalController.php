beed683bea973eb877a6dd6cbaabdee2a1e3a757
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-11-20 20:24:55 +0000

    Chore: journal index method

---
+++ app/Http/Controllers/JournalController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Journal;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class JournalController extends Controller
+{
+    public function index(Request $request)
+    {
+        if ($request->user()->cannot('viewAny', Journal::class)) {
+            abort(403);
+        }
+
+        $journals = Journal::where('user_id', $request->user()->id)
+            ->withCount('comments')
+            ->get();
+
+        return view('journals.index', ['journal' => $journals]);
+    }
+}
