87ed6f0b5a40d19977016857f5acaab0de3eeb38
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-11-20 20:15:31 +0000

    Chore: user viewAll

---
+++ app/Policies/JournalPolicy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Journal;
+use App\Models\User;
+
+class JournalPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function viewAny(User $user)
+    {
+        return $user->role === 'user';
+    }
+}
