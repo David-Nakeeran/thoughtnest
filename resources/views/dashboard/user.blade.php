74fb04c0f547dc5cc4125b73b391919dd4ebc0bf
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-11-23 15:14:59 +0000

    Feat: add link

--- resources/views/dashboard/user.blade.php
+++ resources/views/dashboard/user.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
     <h2>Welcome back {{ $user['name'] }}</h2>
     {{-- button to create a new journal entry --}}
-    {{-- link to view all journals --}}
+    <a href="/journals">View All Journals</a>
 </x-layout>
