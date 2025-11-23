59c603777350bff386b7078136e45ecb29c8986b
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-11-23 15:12:45 +0000

    Feat: basic layout stucture complete

--- resources/views/journals/index.blade.php
+++ resources/views/journals/index.blade.php
@@ -1,5 +1,9 @@
 <x-layout>
-    <h1>User Dashboard</h1>
-    <p>{{ $journal }}</p>
-    <p>{{ $journal->comments_count }}</p>
+    <h1>Journals</h1>
+    @foreach ($journals as $entry)
+        <p>{{ substr($entry['content'], 0, 100) }}...</p>
+        <p>{{ $entry['comments_count'] }} therapist comments</p>
+        <p>Created at: {{ date_format($entry['created_at'], 'd / m / Y') }}</p>
+        <a href="/journals/{{ $entry['id'] }}">View Journal Entry</a>
+    @endforeach
 </x-layout>
