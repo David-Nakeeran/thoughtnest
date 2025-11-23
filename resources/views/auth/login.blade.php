71a2640f8929c2a9452a1e676b247535969a3816
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-11-23 15:13:07 +0000

    Removed: unused code block

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,8 +5,6 @@
         <input type="email" id="email" name="email" required>
         <label for="password">Password</label>
         <input type="password" id="password" name="password" required>
-        <label for="password_confirmation">Confirm Password</label>
-        <input type="password" id="password_confirmation" name="password_confirmation" required>
         <button type="submit">Login</button>
     </form>
 </x-layout>
