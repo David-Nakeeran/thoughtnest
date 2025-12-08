8a3e8b4bed70381a89310c53bf885a9027a904d8
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-12-08 16:45:42 +0000

    Feat: reusable user journal form

---
+++ resources/views/components/journal-form.blade.php
@@ -0,0 +1,18 @@
+@props(['update' => false, 'journal' => null])
+
+@if (!$update)
+    <form method="POST" action="/journals" class="flex flex-col">
+        @csrf
+        <label for="content">Journal Entry</label>
+        <textarea type="text" id="content" name="content" required></textarea>
+        <button type="submit">Submit</button>
+    </form>
+@else
+    <form method="POST" action="/journals/{{ $journal->id }}" class="flex flex-col">
+        @csrf
+        @method('PATCH')
+        <label for="content">Update Journal Entry</label>
+        <textarea type="text" id="content" name="content" required>{{ $journal->content }}</textarea>
+        <button type="submit">Submit</button>
+    </form>
+@endif
