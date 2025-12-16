6ded5f80575288ca99b570e1b65ac142a3ed3ef0
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-12-16 15:43:03 +0000

    Todo: finish basic structure

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,5 +2,15 @@
     <h1>Admin assign users page</h1>
     @foreach ($unassignedUsers as $user)
         <p>{{ $user->name }}</p>
+        <form method="POST" action="">
+            @csrf
+            <label for="therapist-select">Select therapist</label>
+            <select name="therapist" id="therapist-select">
+                @foreach ($therapists as $therapist)
+                    <option value="{{ $therapist->id }}">{{ $therapist->name }}</option>
+                @endforeach
+            </select>
+            <button type="submit">Assign Therapist</button>
+        </form>
     @endforeach
 </x-layout>
