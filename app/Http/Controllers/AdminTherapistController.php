00fcab4b8321de2fa3b5ace649cc39f76526f915
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-12-22 13:17:24 +0000

    Chore: store therapist asssignment

--- app/Http/Controllers/AdminTherapistController.php
+++ app/Http/Controllers/AdminTherapistController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTherapistAssignment;
+use App\Http\Requests\StoreTherapistAssignmentRequest;
 use App\Models\TherapistAssignment;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
@@ -18,5 +20,17 @@ class AdminTherapistController extends Controller
         return view('admin.index', ['unassignedUsers' => $users, 'therapists' => $therapists]);
     }
 
-    public function store(Request $request) {}
+    public function store(StoreTherapistAssignmentRequest $request, User $user)
+    {
+        $therapistId = $request->validated();
+
+        $createTherapistAssignment = TherapistAssignment::create([
+            'user_id' => $user->id,
+            'therapist_id' => $therapistId['therapist']
+        ]);
+
+        $request->session()->flash('success', 'Therapist assigned.');
+
+        return redirect('/therapist-assignments');
+    }
 }
