cd335bbed1160032b17bf3ffc27215a6d963802e
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-12-30 16:24:40 +0000

    Chore: therapist view user journal policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Models\TherapistAssignment;
 use App\Models\User;
 
 class UserPolicy
@@ -17,5 +18,10 @@ class UserPolicy
     public function create(User $user)
     {
         return $user->role === 'admin';
+    }
+
+    public function viewJournals(User $therapist, User $user)
+    {
+        return $therapist->role === 'therapist' && TherapistAssignment::where('therapist_id', '=', $therapist->id)->where('user_id', '=', $user->id)->exists();
     }
 }
