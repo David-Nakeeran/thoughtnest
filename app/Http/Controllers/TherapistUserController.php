798990e27fddfd8551899a414a111a0ea20d550a
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-12-31 15:06:42 +0000

    Chore: index method

--- app/Http/Controllers/TherapistUserController.php
+++ app/Http/Controllers/TherapistUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Journal;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -12,5 +13,12 @@ class TherapistUserController extends Controller
         if ($request->user()->cannot('viewJournals', $user)) {
             abort(403);
         }
+
+        $journals = Journal::where('user_id', $user->id)
+            ->with('comments')
+            ->latest()
+            ->paginate(6);
+
+        return view('therapist.users.journals.index', ['journals' => $journals]);
     }
 }
