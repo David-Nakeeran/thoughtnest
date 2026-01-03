dfc1ea4e5ed867c8913f9b58d83cd01495966199
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2026-01-03 15:25:36 +0000

    Update: unstyled journal entry snippets

--- resources/views/therapist/users/journals/index.blade.php
+++ resources/views/therapist/users/journals/index.blade.php
@@ -1,7 +1,9 @@
 <x-layout>
     <h1>Index</h1>
     @foreach ($journals as $entry)
-        <p>{{ $entry['content'] }}</p>
+        <p>{{ substr($entry['content'], 0, 100) }}...</p>
+        <p>{{ $entry['comments_count'] }} therapist comments</p>
         <p>Created at: {{ date_format($entry['created_at'], 'd / m / Y') }}</p>
+        <a href="/therapist/users/{{ $user->id }}/journals/{{ $entry->id }}">View Entry</a>
     @endforeach
 </x-layout>
